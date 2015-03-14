6f839638edaa1eda485b61cad6ed95bd5d5367f7
Daniel Pesqueda <dpesqueda@csumb.edu>
2015-03-14 14:57:19 -0700

    SMALL TYPO

--- helloworld.php
+++ helloworld.php
@@ -3,7 +3,7 @@ $username = ($_POST['username']);
 $password = ($_POST['password']);
 
 echo "This is a POST response from the server and your username is: ".$username." and your".
-"password is: ".$password;
+" password is: ".$password;
 //echo exec("whoami");
 
 //passthru('python test.py 1 2>&1');
