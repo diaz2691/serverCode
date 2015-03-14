7fd53efc944a310808a64b8ed548c8c2721dcec6
Daniel Pesqueda <dpesqueda@csumb.edu>
2015-03-14 14:54:30 -0700

    CAN NOW RESPOND CORRECTLY FROM SERVER

--- helloworld.php
+++ helloworld.php
@@ -1,12 +1,9 @@
 <?php
-// $username = json_decode($_POST['username']);
-// $password = json_decode($_POST['password']);
+$username = ($_POST['username']);
+$password = ($_POST['password']);
 
-foreach (getallheaders() as $name => $value){
-	echo "$name: $value\n";
-}
-echo ($_POST['username']);
-//echo $username." ".$password;
+echo "This is a POST response from the server and your username is: ".$username." and your".
+"password is: ".$password;
 //echo exec("whoami");
 
 //passthru('python test.py 1 2>&1');
